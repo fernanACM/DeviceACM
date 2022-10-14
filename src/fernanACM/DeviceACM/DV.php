54b6d33fd020a073c6c12e29f5b4c020ce217f65
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-10-14 01:56:11 -0500

    REMAKE DeviceACM

--- src/fernanACM/DeviceACM/DV.php
+++ src/fernanACM/DeviceACM/DV.php
@@ -1,5 +1,13 @@
 <?php
 
+#      _       ____   __  __ 
+#     / \     / ___| |  \/  |
+#    / _ \   | |     | |\/| |
+#   / ___ \  | |___  | |  | |
+#  /_/   \_\  \____| |_|  |_|
+# The creator of this plugin was fernanACM.
+# https://github.com/fernanACM
+
 namespace fernanACM\DeviceACM;
 
 use pocketmine\Server;
@@ -7,35 +15,84 @@ use pocketmine\player\Player;
 
 use pocketmine\plugin\PluginBase;
 
+use pocketmine\utils\Config;
+
 use pocketmine\network\mcpe\protocol\types\DeviceOS;
 
-use fernanACM\DeviceACM\DeviceTask;
+use fernanACM\DeviceACM\task\DeviceTask;
 
-class DV extends PluginBase {
-  
-  public static $instance;
-	
-  public function onLoad(): void{
-     self::$instance = $this;
-  }
-	
-  public function onEnable(): void{
-     $this->getScheduler()->scheduleRepeatingTask(new DeviceTask($this), 11);
-  }
-	
-  public static function getInstance(){
-  	return self::$instance;
-  }
-    
-  public function getPlayerPlatform(Player $player): string {
+use fernanACM\DeviceACM\utils\PluginUtils;
+
+use fernanACM\DeviceACM\faction\FactionSupport;
+use fernanACM\DeviceACM\faction\support\PiggyFactionSupport;
+use fernanACM\DeviceACM\faction\support\SimpleFactionSupport;
+
+class DV extends PluginBase{
+
+    # Config
+    public Config $config;
+    # CheckConfig
+    public const CONFIG_VERSION = "1.0.0";
+    # Instance
+    public static $instance;
+    # Faction
+	public static $factionType;
+
+    public function onLoad(): void{
+        self::$instance = $this;
+    }
+
+    public function onEnable(): void{
+        $this->loadFiles();
+        $this->loadCheck();
+        $this->loadFaction();
+        $this->loadEvents();
+    }
+
+    public function loadEvents(){
+        Server::getInstance()->getPluginManager()->registerEvents(new Event($this), $this);
+        $this->getScheduler()->scheduleRepeatingTask(new DeviceTask($this), 11);
+    }
+
+    public function loadFiles(){
+        $this->saveResource("config.yml");
+        $this->config = new Config($this->getDataFolder() . "config.yml");
+    }
+
+    public function loadCheck(){
+        # CONFIG
+        if((!$this->config->exists("config-version")) || ($this->config->get("config-version") != self::CONFIG_VERSION)){
+            rename($this->getDataFolder() . "config.yml", $this->getDataFolder() . "config_old.yml");
+            $this->saveResource("config.yml");
+            $this->getLogger()->critical("Your configuration file is outdated.");
+            $this->getLogger()->notice("Your old configuration has been saved as config_old.yml and a new configuration file has been generated. Please update accordingly.");
+        }
+    }
+
+    public function loadFaction(){
+        if($this->config->get("FactionSupport") === true){
+            foreach(Server::getInstance()->getPluginManager()->getPlugins() as $plugin){
+                if($plugin instanceof \DaPigGuy\PiggyFactions\PiggyFactions){
+                    $this->getLogger()->notice("PiggyFactions factions support has been loaded.");
+                    self::$factionType = new PiggyFactionSupport($plugin);
+                    return;
+                }
+                if($plugin instanceof \Ayzrix\SimpleFaction\Main){
+                    $this->getLogger()->notice("SimpleFaction factions support has been loaded.");
+                    self::$factionType = new SimpleFactionSupport($plugin);
+                    return;
+                }
+            }
+        }
+        $this->getLogger()->critical("Faction support has been canceled because it has not been found");
+    }
+
+    public static function getPlayerPlatform(Player $player): string{
         $extraData = $player->getPlayerInfo()->getExtraData();
-
-        if ($extraData["DeviceOS"] === DeviceOS::ANDROID && $extraData["DeviceModel"] === "") {
+        if($extraData["DeviceOS"] === DeviceOS::ANDROID && $extraData["DeviceModel"] === ""){
             return "Linux";
         }
-
-        return match ($extraData["DeviceOS"])
-        {
+        return match ($extraData["DeviceOS"]){
             DeviceOS::ANDROID => "Android",
             DeviceOS::IOS => "iOS",
             DeviceOS::OSX => "macOS",
@@ -52,5 +109,17 @@ class DV extends PluginBase {
             DeviceOS::WINDOWS_PHONE => "Windows Phone",
             default => "Unknown"
         };
+    }
+
+    public static function getScore(Player $player, string $key){
+        return PluginUtils::DeviceCode($player, DV::getInstance()->config->getNested($key, $key));
+    }
+
+    public static function getFactionType(): FactionSupport{
+        return self::$factionType;
+    }
+
+    public static function getInstance(): DV{
+        return self::$instance;
     }
 }
